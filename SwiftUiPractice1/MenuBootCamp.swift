3351018cb2aab32dd140b0d3fd773c88c1f1fcdd
dhanuiOS <117150188+dhanuiOS@users.noreply.github.com>
2025-07-11 11:53:41 +0530

    Added Menu Code

---
+++ SwiftUiPractice1/MenuBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  MenuBootCamp.swift
+//  SwiftUiPractice1
+//
+//  Created by PINNINTI DHANANJAYARAO on 11/07/25.
+//
+
+import SwiftUI
+
+struct MenuBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuBootCamp()
+}
